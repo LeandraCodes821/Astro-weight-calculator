15dc151a7b784f6978a68cfb3b5cb4e9d84078c1
LeandraCodes821 <Ms.knowitall2336@gmail.com>
2022-01-31 14:17:19 -0800

    created dropdown menu of planets

--- main.js
+++ main.js
@@ -1,1 +1,42 @@
 // Write your JavaScript code here!
+var planets = [ 
+    ['Pluto', 0.06], 
+    ['Neptune', 1.148], 
+    ['Uranus', 0.917], 
+    ['Saturn', 1.139], 
+    ['Jupiter', 2.640], 
+    ['Mars', 0.3895], 
+    ['Moon', 0.1655], 
+    ['Earth', 1], 
+    ['Venus', 0.9032], 
+    ['Mercury', 0.377], 
+    ['Sun', 27.9] 
+];
+    var populateDropdown = document.getElementById("planets");
+        planets.foreach(planet => {
+            var option = document.createElement("option");
+            option.value = planet[0];
+            option.innerText = planet[0];
+            populateDropdown.appendChild(option)
+        });
+    populateDropdown();
+        
+    function calculateWeight(weight, planetName) { 
+        for(var i = 0; i < planets.length; i++){
+            if (planets[i][0] == planetName) {
+                return parseInt(weight) * planets[i][1];
+                }
+            }
+        }
+        function handleClickEvent(e) {
+            var userWeight = 
+        parseInt(document.getElementById(userWeight).value);
+            var planetName = 
+            document.querySelectorAll("planets option")
+            [document.getElementById("planets").selectedIndex].textContent;
+            var result = calculateWeight(userWeight, planetName);
+            document.getElementById("output").innerHTML = "If you were on" + planetName + "you would weigh" + result + "lbs!";
+        } 
+        document.getElementById("calculatebutton").addEventListener("click",handleClickEvent);
+        
+    
